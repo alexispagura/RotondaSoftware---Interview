c3759f37cda719f71758c1d646f767e551389577
Alexis Pagura <alexis.pagura@codexlabscorp.com>
2024-01-17 09:36:55 -0300

    Add comment to error-alarm exercise

--- 02-error-alarm/main.js
+++ 02-error-alarm/main.js
@@ -1,5 +1,7 @@
-const ONE_MINUTE_IN_MS = 1 * 60 * 1000;
+// Notifications can be stores in the database or in a text file, we just need a way to check the timestamp of the last one.
 let lastNotificationTimestamp = null;
+
+const ONE_MINUTE_IN_MS = 1 * 60 * 1000;
 
 function logError(error) {
   const currentTimestamp = getCurrentDateTime();
